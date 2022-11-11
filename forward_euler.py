245befa8cabe534208190fb4e1c42618a14408ee
tor <torliljegren@icloud.com>
2022-11-11 21:12:32 +0100

    Choose to plot or write out

--- forward_euler.py
+++ forward_euler.py
@@ -1,10 +1,12 @@
 from math import *
 from time import perf_counter
 from xlsxwriter import Workbook
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 
-XLS_FILENAME = 'fw_euler_output.xls'
+XLS_FILENAME = 'fw_euler_output.xls' # file name of output
+CREATE_PLOT = True # program creates a pyplot if True
+CREATE_XLSX = False # program creates an Excel doc with point data in program directory if True
 
 
 # the function on the right hand side of the differential equation y'=f(x,y)
@@ -22,17 +24,12 @@ y0 = 5
 # stop at xstop
 xstop = 5
 
-# flag for fail and for which x
-fail = False
-xfail = 0
-
 # variables for x and y
 x = x0
 y = y0
 
 # list containing the points of the solution curve
 xylist = [(x0, y0)]
-
 
 print('Calculating points on solution curve')
 
@@ -42,7 +39,12 @@ t1 = perf_counter()
 # calculate the points in the solution curve
 while x <= xstop:
     # compute the next y (y_n+1 = y_n + y'(x_n)) * h)
-    y += f(x, y) * h
+    try:
+        y += f(x, y) * h
+    except ValueError:
+        print('Error in calculation. y might have grown too large')
+    except ZeroDivisionError:
+        print('Error in calculation. Attempted to divide by zero')
 
     # increment x (to x_n+1)
     x += h
@@ -55,23 +57,26 @@ t2 = perf_counter()
 
 print('Done. Calculated', len(xylist), 'points in', round(t2 - t1, ndigits=4), 'seconds')
 
-# open a xls doc for writing points to
-print('Creating Excel sheet:', XLS_FILENAME + "...")
-wb = Workbook(XLS_FILENAME)
-ws = wb.add_worksheet(name='Output')
+if CREATE_XLSX:
+    # open a xls doc for writing points to
+    print('Creating Excel sheet:', XLS_FILENAME + "...")
+    wb = Workbook(XLS_FILENAME)
+    ws = wb.add_worksheet(name='Output')
 
-ws.write_column('A1', data=[point[0] for point in xylist])
-ws.write_column('B1', data=[point[1] for point in xylist])
+    ws.write_column('A1', data=[point[0] for point in xylist])
+    ws.write_column('B1', data=[point[1] for point in xylist])
 
-wb.close()
-print('Done. Opening plot...')
+    wb.close()
+    print('Done')
 
-# prepare a pyplot to display the curve
-fig, ax = plt.subplots()
+if CREATE_PLOT:
+    print('Preparing plot...')
+    # prepare a pyplot to display the curve
+    fig, ax = plt.subplots()
 
-ax.set(xlabel='x', ylabel='y',
-       title=None)
-ax.grid()
-plt.title('Lösningskurva')
-ax.plot([point[0] for point in xylist], [point[1] for point in xylist])
-plt.show()
+    ax.set(xlabel='x', ylabel='y',
+           title=None)
+    ax.grid()
+    plt.title('Lösningskurva')
+    ax.plot([point[0] for point in xylist], [point[1] for point in xylist])
+    plt.show()
