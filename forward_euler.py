b123f258d218e7df8cc017a98aee22a10edeec19
tor <torliljegren@icloud.com>
2022-11-17 23:49:09 +0100

    Reqs updated

--- forward_euler.py
+++ forward_euler.py
@@ -4,27 +4,27 @@ from xlsxwriter import Workbook
 # import numpy as np
 import matplotlib.pyplot as plt
 
-XLS_FILENAME = 'fw_euler_output.xls' # file name of output
-CREATE_PLOT = True # program creates a pyplot with the solution curve if True
-CREATE_XLSX = True # program creates an Excel doc with point data in program directory if True
+XLS_FILENAME = 'fw_euler_output.xls'  # file name of output
+CREATE_PLOT = True  # program creates a pyplot with the solution curve if True
+CREATE_XLSX = False  # program creates an Excel doc with point data in program directory if True
 
 
 # the function on the right hand side of the differential equation y'=f(x,y)
 def f(x: float, y: float) -> float:
-    return log(x)-log(y) # (2,0; 2,25)
+    # return log(x)-log(y)  # (2,0; 2,25)
     # return -x*y/10+10
-    # return 2*sin(y) - x
+    return 2*sin(y) - x
 
 
-# step length (recommended 10^-7 < h < 10^-2)
-h = 10**-3
+# step length (recommended 10^-6 < h < 10^-2)
+h = 10**-1
 
 # start at (x0, y0)
 x0 = 0
 y0 = 5
 
 # stop at xstop
-xstop = 10
+xstop = 6
 
 # variables for x and y
 x = x0
@@ -44,7 +44,7 @@ while x <= xstop:
     try:
         y += f(x, y) * h
     except ValueError:
-        print('Error in calculation. y might have grown too large')
+        print('Error in calculation. y might have grown too large or x outside domain')
     except ZeroDivisionError:
         print('Error in calculation. Attempted to divide by zero')
 
